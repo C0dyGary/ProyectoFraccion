7a518eb6c83fa438d2e2fa4b9f9cc90adecb9faa
Gary Rosa Valencia <asenvhale@gmail.com>
2022-11-15 11:01:59 -0400

    Primere commit

---
+++ Fraccion.java
@@ -0,0 +1,12 @@
+public class Fraccion {
+  private int numerador;
+  private int denominador;
+
+  public Fraccion(int numerador, int denominador) {
+    this.numerador = numerador;
+    this.denominador = denominador;
+  }
+  // el prime usuraio hace le metodo suma
+  // el segundo le metodo multiplica
+  // el tersero resta
+}
